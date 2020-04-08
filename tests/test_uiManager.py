4bed9d14480be06e46b77ae17261bb5a7c4070c1
Jimmy Girardet <ijkl@netc.fr>
2020-04-08 12:40:28 +0200

    uiman

--- tests/test_uiManager.py
+++ tests/test_uiManager.py
@@ -8,28 +8,8 @@ def uiman() -> UiManager:
     return UiManager()
 
 
-def test_uiman(uiman):
+def test_uiman(uiman, qtbot):
     assert True
 
 
-# class TestUiManager:
-#     def test_menuFlottantStabylo(self, uiman: UiManager, qtbot):
-#         a = QObject()
-#         with qtbot.waitSignal(uiman.menuFlottantStabyloChanged):
-#             uiman.menuFlottantStabylo = a
-#         assert uiman.menuFlottantStabylo == a
-#
-#     def test_menuFlottantText(self, uiman: UiManager, qtbot):
-#         a = QObject()
-#         with qtbot.waitSignal(uiman.menuFlottantTextChanged):
-#             uiman.menuFlottantText = a
-#         assert uiman.menuFlottantText == a
-#
-#     def test_menuTarget(self, uiman: UiManager, qtbot):
-#         a = QObject()
-#         with qtbot.waitSignal(uiman.menuTargetChanged):
-#             uiman.menuTarget = a
-#         assert uiman.menuTarget == a
-#
-#         with qtbot.assertNotEmitted(uiman.menuTargetChanged):
-#             uiman.menuTarget = a
+# class TestUiMan

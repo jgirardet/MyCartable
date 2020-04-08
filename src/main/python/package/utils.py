50b116d8ef8874a35cc13669256a4ad568a049e5
Jimmy Girardet <ijkl@netc.fr>
2020-04-08 08:29:09 +0200

    remove QApllication()

--- src/main/python/package/utils.py
+++ src/main/python/package/utils.py
@@ -3,7 +3,8 @@ from PySide2.QtWidgets import QApplication
 
 
 def create_singleshot(fn):
-    timer = QTimer(QApplication.instance() or QApplication())
+    # timer = QTimer(QApplication.instance() or QApplication())
+    timer = QTimer(QApplication.instance())
     timer.setSingleShot(True)
     timer.timeout.connect(fn)
     return timer

84828a33af2b2cc701025d9f5333aef750464351
Jimmy Girardet <ijkl@netc.fr>
2020-04-25 19:19:39 +0200

    test running

--- run.py
+++ run.py
@@ -11,9 +11,9 @@ from pathlib import Path
 
 PACKAGE = "MyCartable"
 PACKAGE_ENV = "MyCartableEnv"
-QT_VERSION="5.14.1"
+QT_VERSION = "5.14.1"
 ROOT = Path(__file__).parent
-QT_PATH= ROOT / QT_VERSION
+QT_PATH = ROOT / QT_VERSION
 DIST = ROOT / "dist" / PACKAGE
 QMLTESTS = ROOT / "build" / "qml_tests"
 
@@ -21,51 +21,73 @@ QMLTESTS = ROOT / "build" / "qml_tests"
 def get_env():
     env = os.environ
     if sys.platform == "linux":
-        env['PATH'] = str(QT_PATH /"gcc_64"/"bin") + ":" + env['PATH']
+        env["PATH"] = str(QT_PATH / "gcc_64" / "bin") + ":" + env["PATH"]
     return env
 
-def get_shell():
-    return os.environ.get('SHELL', None)
 
-def runCommand(command, cwd = str(ROOT), sleep_time=0.2, force_env=True):
+def get_shell():
+    return os.environ.get("SHELL", None)
+
+
+def runCommand(
+    command, cwd=str(ROOT), sleep_time=0.2, force_env=True,
+):
     print(f"##### running: {command} #####")
     env = get_env()
-    if os.environ.get('CONDA_DEFAULT_ENV',None) != PACKAGE_ENV and force_env:
+    if os.environ.get("CONDA_DEFAULT_ENV", None) != PACKAGE_ENV and force_env:
         command = f"conda run -n {PACKAGE_ENV} " + command
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,  executable=get_shell(), cwd=cwd, env=env)
+    process = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        executable=get_shell(),
+        cwd=cwd,
+        env=env,
+    )
     while process.poll() is None:
         for line in process.stdout:
-            print(line.strip(b'\n').decode())
+            print(line.strip(b"\n").decode())
         time.sleep(sleep_time)
     if process.returncode == 0:
-        print(f"##### finished: {command}  ==>>  OK  with return code {process.returncode} #####")
+        print(
+            f"##### finished: {command}  ==>>  OK  with return code {process.returncode} #####"
+        )
+        return True
     else:
-        print(f"##### finished: {command}  ==>>  ECHEC  with return code {process.returncode} #####")
-        sys.exit(process.returncode)
+        print(
+            f"##### finished: {command}  ==>>  ECHEC  with return code {process.returncode} #####"
+        )
+        return sys.exit(process.returncode)
+
 
 def cmd_black():
     runCommand("python -m black")
+
 
 def cmd_cov():
     pytest_cache = ROOT / ".pytest_cache"
     if pytest_cache.exists():
         shutil.rmtree(pytest_cache)
-    runCommand('coverage run --rcfile=.coveragerc -m pytest')
-    runCommand('coverage report')
+    runCommand("coverage run --rcfile=.coveragerc -m pytest")
+    runCommand("coverage report")
 
 
 def cmd_cov_html():
     cmd_cov()
     runCommand("coverage html")
     html = ROOT / "htmlcov" / "index.html"
-    runCommand(f'firefox {html} &')
+    runCommand(f"firefox {html} &")
+
 
 def cmd_build_binary_as_dir():
     runCommand("pyinstaller  scripts/dir.spec --clean -y")
     cmd_test_binary_as_dir()
 
+
 def cmd_install():
     runCommand(f"conda env create -f {PACKAGE_ENV}.yml", force_env=False)
+
 
 def cmd_install_qt():
     if QT_PATH.exists():
@@ -87,22 +109,27 @@ def cmd_runCommand():
 def cmd_run_dist():
     executable = PACKAGE
     if sys.platform == "win32":
-        executable   = executable + ".exe"
-    runCommand(str(DIST/executable))
+        executable = executable + ".exe"
+    runCommand(str(DIST / executable))
+
 
 def cmd_setup_qml():
     if QMLTESTS.exists():
         shutil.rmtree(QMLTESTS)
     runCommand("python tests/qml_tests/create-js-data.py")
     if sys.platform == "linux":
-        runCommand(f"qmake -o {QMLTESTS}/Makefile tests/qml_tests/qml_tests.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug")
+        runCommand(
+            f"qmake -o {QMLTESTS}/Makefile tests/qml_tests/qml_tests.pro -spec linux-g++ CONFIG+=debug CONFIG+=qml_debug"
+        )
 
 
 def cmd_test_binary_as_dir():
     runCommand("python scripts/test_build_dir.py")
 
+
 def cmd_test_python():
     runCommand("python -m pytest -s")
+
 
 def cmd_test_qml():
     qml_tests = "qml_tests"
@@ -111,19 +138,21 @@ def cmd_test_qml():
         runCommand("make -C build/qml_tests")
     runCommand(str(QMLTESTS / qml_tests))
 
+
 def cmd_test_qml_reset():
     cmd_setup_qml()
     cmd_test_qml()
 
+
 def build_commands():
     res = {}
     for i, j in globals().items():
-        if callable(j) and i.startswith('cmd_'):
+        if callable(j) and i.startswith("cmd_"):
             res[i[4:]] = j
     return res
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     com = ""
     commands = build_commands()
     com = sys.argv[-1]
--- scripts/test_build_dir.py
+++ scripts/test_build_dir.py
@@ -1,11 +1,19 @@
 from pathlib import Path
 
 import pytest
+import sys
+import subprocess
+import platform
 
+# from run import runCommand, cmd_run_dist
 
 root = Path(__file__).parents[1]
-binary = root  / "binary"
+sys.path = [str(root)] + sys.path
+binary = root / "binary"
 dist = root / "dist" / "MyCartable"
+filename = "MyCartable" if sys.platform == "linux" else "MyCartable.exe"
+exe = dist / filename
+
 
 def test_binary_included():
     dist_binary = dist / "binary"
@@ -13,5 +21,41 @@ def test_binary_included():
         assert x.relative_to(dist) == y.relative_to(root)
 
 
-if __name__ == '__main__':
+def test_run_exec_in_dir():
+    STARTUPINFO = None
+    if platform.system == "Windows":
+        STARTUPINFO = subprocess.STARTUPINFO()
+        STARTUPINFO.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        STARTUPINFO.wShowWindow = subprocess.SW_HIDE
+    proc = subprocess.Popen(
+        [str(exe)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        startupinfo=STARTUPINFO,
+    )
+
+    try:
+        proc.wait(timeout=10)
+    except subprocess.TimeoutExpired:
+        assert proc.poll() is None
+        print("execution sans problème après 10 secondes")
+        proc.kill()
+        assert True
+
+    else:
+        print("Le programme s'est intéromput plus tôt")
+        try:
+            out, err = proc.communicate(timeout=5)
+        except subprocess.TimeoutExpired:
+            print("Le programme est bloqué")
+            print(proc.stdout.read().decode())
+            print(proc.stderr.read().decode())
+            # on quite
+        else:
+            print("Message d'erreur")
+            print(out.decode())
+        sys.exit(-1)
+
+
+if __name__ == "__main__":
     pytest.main([str(Path(__file__).parent)])

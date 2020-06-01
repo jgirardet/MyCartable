3a26ded96030ab340426de3c85675df4125d6b69
Jimmy Girardet <ijkl@netc.fr>
2020-06-01 13:04:26 +0200

    generator

--- src/python/package/odt_creator.py
+++ src/python/package/odt_creator.py
@@ -21,15 +21,15 @@ doc.styles.addElement(p_style)
 h1_style = Style(
     name="h1_style",
     family="paragraph",
-    parentstylename=p_style,
+    parentstylename="p_style",
     defaultoutlinelevel="1",
 )
 h1_text_prop = TextProperties(
     attributes={
-        # "fontname": "Verdana",
-        # "fontfamily": "Verdana",
-        # "fontstylename": "Normal",
-        # "fontpitch": "variable",
+        "fontname": "Verdana",
+        "fontfamily": "Verdana",
+        "fontstylename": "Normal",
+        "fontpitch": "variable",
         "fontsize": "36pt",
     }
 )
@@ -37,7 +37,7 @@ h1_style.addElement(h1_text_prop)
 doc.styles.addElement(h1_style)
 
 intro = P(stylename=p_style)
-intro.addText("coucou blabla")
+intro.addText("coucou blabeela")
 doc.text.addElement(intro)
 
 titre1 = H(stylename=h1_style, outlinelevel="1")
@@ -45,4 +45,20 @@ titre1.addText("titre 1")
 doc.text.addElement(titre1)
 
 
+def new_h1(text):
+    elem = H(stylename=h1_style, outlinelevel="1")
+    elem.addText(text)
+    doc.text.addElement(elem)
+
+def new_p(text):
+    elem = P(stylename=p_style)
+    elem.addText(text)
+    doc.text.addElement(elem)
+
+
+new_h1("mpkojlihkugjy")
+new_p("mpkojlihkugjy")
+
 doc.save("/home/jimmy/bla.odt")
+import subprocess
+subprocess.run("xdg-open /home/jimmy/bla.odt".split())

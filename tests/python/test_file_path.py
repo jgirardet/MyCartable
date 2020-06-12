4b0ae0ddd57f609082aa5e62c44da9ce3e8cb527
Jimmy Girardet <ijkl@netc.fr>
2020-06-12 18:23:07 +0200

    refactor: file_path

---
+++ tests/python/test_file_path.py
@@ -0,0 +1,29 @@
+import shutil
+from pathlib import Path
+
+from package.files_path import root_data, files, tmp_files
+from PySide2.QtCore import QStandardPaths
+
+
+def test_root_data():
+    appdata = Path(QStandardPaths.writableLocation(QStandardPaths.AppDataLocation))
+    rrr = root_data()
+    assert rrr == appdata
+    shutil.rmtree(rrr)
+    rrr = root_data()
+    assert rrr.is_dir()
+
+
+def test_files():
+    root = root_data()
+    shutil.rmtree(root)
+    fil = files(root)
+    assert fil.is_dir()
+    assert fil == root / "files"
+
+
+def test_tmp_files():
+    tt = tmp_files()
+    shutil.rmtree(tt)
+    tt = tmp_files()
+    assert tt.is_dir()

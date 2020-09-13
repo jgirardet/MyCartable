139a883e2c31538d85fa2c13dc58bbab24b2fb20
Jimmy Girardet <ijkl@netc.fr>
2020-09-14 00:18:14 +0200

    fix: revert export equation
    Le changement de font ne marche pas avec la conversion libreoffice. Voir pour intégrer la police à la volée ?

--- src/mycartable/package/constantes.py
+++ src/mycartable/package/constantes.py
@@ -37,7 +37,7 @@ ANNOTATION_TEXT_BG_OPACITY = 0.5
 
 MONOSPACED_FONTS = [
     "Liberation Mono",
-    "Code New Roman",
+    # "Code New Roman",
     "Courier",
     "Courier New",
     "Noto Mono",

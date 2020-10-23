f67318bb93b76d99a033e30f15e20ad7850a1474
Jimmy Girardet <ijkl@netc.fr>
2020-10-23 09:55:03 +0200

    add: migrator

--- src/mycartable/package/migrate.py
+++ src/mycartable/package/migrate.py
@@ -1,12 +1,13 @@
 """
 Ici sont expliquées les migrations chaque changement dans la ddb.
 """
+from contextlib import contextmanager
 from pathlib import Path
 from typing import Union
 
 from package.database.base_db import Schema
 from package.utils import Version
-from pony.orm import Database
+from pony.orm import Database, db_session
 
 """
 type:
@@ -31,3 +32,43 @@ def get_db_version(file: Union[Database, str, Path]):
         return Version("1.2.2")
     else:
         return version
+
+
+migrations = {"1.3.0": ["ALTER TABLE Annotation ADD points TEXT"]}
+
+
+class Migrator:
+    def __init__(self, db: Database, version: Version, migrations: dict):
+        self.db = db
+        self.version = version
+        self.migrations = migrations
+
+    def select_migrations(self):
+        selected = []
+        for ver, migs in self.migrations.items():
+            version = Version(ver)
+            if version < self.version:
+                selected += migs
+        return selected
+
+    def process_one(self, line: str):
+        with db_session:
+            self.db.execute(line)
+
+    def process_migrations(self, mig_list: list):
+        for mig in mig_list:
+            self.process_one(mig)
+
+    def __call__(self):
+        migrations = self.select_migrations()
+        self.process_migrations(migrations)
+
+
+#
+# def apply_new_schema_version(db, version):
+#     with db_session:
+#         db.Configuration.add
+#
+#
+# def apply_all_migrations(filename):
+#     pass
--- tests/python/test_migrate.py
+++ tests/python/test_migrate.py
@@ -1,7 +1,7 @@
 import pytest
 from package.database import init_database
 from package.database.base_db import Schema
-from package.migrate import get_db_version
+from package.migrate import get_db_version, Migrator
 from package.utils import Version
 from pony.orm import Database, PrimaryKey, Required, Optional, db_session
 
@@ -25,3 +25,77 @@ def test_get_db_version(mdb):
     s = Schema(file=mdb)
     s.version = Version("3.5.4")
     assert get_db_version(mdb) == Version("3.5.4")
+
+
+class TestMigrator:
+    migrations = {
+        "1.3.0": ["""ALTER TABLE bla ADD "texta" TEXT"""],
+        "1.3.1": [
+            """ALTER TABLE bla ADD "textb" TEXT""",
+            """ALTER TABLE bla ADD "textc" TEXT""",
+        ],
+        "1.3.4": [
+            """ALTER TABLE bla ADD "textd" TEXT""",
+        ],
+    }
+
+    @pytest.fixture()
+    def onetable(self, mdb):
+        with db_session:
+            mdb.execute("""CREATE TABLE "bla" ("key" TEXT NOT NULL PRIMARY KEY);""")
+            mdb.execute("""INSERT INTO "bla" VALUES("prems")""")
+        return mdb
+
+    def test_one_migration(self, onetable):
+        m = Migrator(
+            onetable, Version("1"), {"0.9": ["""INSERT INTO "bla" VALUES("deux")"""]}
+        )
+        m()
+        with db_session:
+            assert m.db.execute("select * from bla").fetchall() == [
+                ("prems",),
+                ("deux",),
+            ]
+
+    def test_many_migrations_not_all(self, onetable):
+        m = Migrator(onetable, Version("1.3.2"), self.migrations)
+        m()
+        with db_session:
+            assert (
+                m.db.execute("select * from sqlite_master").fetchone()[4]
+                == 'CREATE TABLE "bla" ("key" TEXT NOT NULL PRIMARY KEY, "texta" TEXT, "textb" TEXT, "textc" TEXT)'
+            )
+
+    def test_many_migrations_all(self, onetable):
+        m = Migrator(onetable, Version("1.3.5"), self.migrations)
+        m()
+        with db_session:
+            assert (
+                m.db.execute("select * from sqlite_master").fetchone()[4]
+                == 'CREATE TABLE "bla" ("key" TEXT NOT NULL PRIMARY KEY, "texta" TEXT, "textb" TEXT, "textc" TEXT, "textd" TEXT)'
+            )
+
+    def test_one_migration_not_null_with_default(self, onetable):
+        m = Migrator(
+            onetable,
+            Version("1"),
+            {"0.9": ["""ALTER TABLE bla ADD "textd" TEXT NOT NULL DEFAULT "a" """]},
+        )
+        m()
+        with db_session:
+            assert m.db.execute("select * from bla").fetchone() == ("prems", "a")
+
+    def test_one_migration_not_null_with_initial_value(self, onetable):
+        m = Migrator(
+            onetable,
+            Version("1"),
+            {
+                "0.9": [
+                    """ALTER TABLE bla ADD "textd" TEXT NOT NULL DEFAULT "";""",
+                    """UPDATE bla SET textd="b";""",
+                ]
+            },
+        )
+        m()
+        with db_session:
+            assert m.db.execute("select * from bla").fetchone() == ("prems", "b")

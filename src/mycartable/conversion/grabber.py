88bfa6128cd84b92636d531a01a92bf5bdbad1d1
Jimmy Girardet <ijkl@netc.fr>
2021-01-10 16:51:36 +0100

    test: fix grabber.py

--- src/mycartable/conversion/grabber.py
+++ src/mycartable/conversion/grabber.py
@@ -1,83 +1,16 @@
-import sys
 import time
-from pathlib import Path
 from typing import Union
 
 from PyQt5.QtCore import (
-    pyqtSlot,
-    QObject,
     QUrl,
     QEventLoop,
     QCoreApplication,
     Qt,
 )
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtQml import qmlRegisterType, QQmlEngine
 from PyQt5.QtQuick import QQuickView
 from loguru import logger
 
 from . import WImage
-
-tmp = Path("/tmp/rien.qml")
-tmp.write_text(
-    """
-import QtQuick 2.15
-//import MyCartable 1.0
-import QtQuick.Window 2.15
-
-Rectangle {
-    id: root
-  //  Section {
-    //    id: section
-   // }
-    height: 200
-    width: 200
-    color: "red"
-    MouseArea {
-        anchors.fill: parent
-        onClicked: {
-            //section.grab(Window.window)
-            print(Window.window)
-            Window.window.grab()
-        }
-    }
-}
-"""
-)
-
-tmp2 = Path("/tmp/rien2.qml")
-tmp2.write_text(
-    """
-import QtQuick 2.15
-//import MyCartable 1.0
-import QtQuick.Window 2.15
-
-Rectangle {
-    id: root
-  //  Section {
-    //    id: section
-   // }
-     property  Item sectionItem
-     width: sectionItem.width
-     height: 50
-    color: "blue"
-    MouseArea {
-        anchors.fill: parent
-        onClicked: {
-            //section.grab(Window.window)
-            print(Window.window)
-            Window.window.grab()
-        }
-    }
-}
-"""
-)
-
-
-class Section(QObject):
-    @pyqtSlot(QObject)
-    def grab(self, win: QQuickView):
-        print("grabbed", win)
 
 
 class Grabber(QQuickView):
@@ -136,15 +69,3 @@ class Grabber(QQuickView):
     def on_grab(self):
         self.image_grabbed = WImage(self.res_grab.image())
         self.close()
-
-
-if __name__ == "__main__":
-    app = QGuiApplication([])
-    qmlRegisterType(Section, "MyCartable", 1, 0, "Section")
-    engine = QQmlEngine()
-    a = Grabber(tmp, engine)
-    # a(tmp)
-
-    res = a()
-    print(res.width())
-    sys.exit(app.exec_())
